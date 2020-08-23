b5128054ee5cccc7c3ad20b31a287c76b4ae4fd8
Keith Curbelo <keithcurbelo@yahoo.com>
2020-08-23 18:38:46 -0400

    fetch images from unspalsh api on load

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 //Unsplash API
-const count = 10;
+const count = 5;
 const apiKey = "";
 const apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+//Get phots from Unsplash API
+const getPhotos = async () => {
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    //catch error
+  }
+};
+
+//On Load
+getPhotos();
